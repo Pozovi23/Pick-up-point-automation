f225ea0b6727394cab1023b816c4b0a0c2eea3d0
Pozovi23 <gleb.zhigalov.2005@mail.ru>
2025-06-09 00:34:52 +0700

    added tests for robot code

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="warehouse_automation",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        'psycopg2-binary>=2.9',
+        'pytest>=7.0',
+    ],
+    python_requires=">=3.9",
+)
